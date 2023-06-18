558c048ce95ea1d6d8fbdb472d758241c2f0407c
kulacs-kristof <98750722+kulacs-kristof@users.noreply.github.com>
2023-06-18 22:07:47 +0200

    Add files via upload

---
+++ Kettlebell/Kettlebell/kettlebell.js
@@ -0,0 +1,12 @@
+﻿function gyakorlatGeneralas() {
+    var gyakorlatok = ["Swing", "Magasra húzás", "Serleg guggolás", "Szélmalom", "Török felállás"];
+    var gyakorlat = document.getElementById("generaltGyakorlat");
+    var ismetles = Math.floor(Math.random() * 30) + 1;
+    var gyakorlatIndex =  Math.floor(Math.random() * gyakorlatok.length) + 1;
+    gyakorlat.innerText = ismetles + " darab " + gyakorlatok[gyakorlatIndex];
+    if (ismetles % 2 == 0) {
+        gyakorlat.style.color = "red";
+    } else {
+        gyakorlat.style.color = "blue";
+    }
+}
