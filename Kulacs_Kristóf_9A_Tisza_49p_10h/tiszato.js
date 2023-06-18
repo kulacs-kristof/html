d8a93b3c01598b92269c90f40051e038a5718a14
kulacs-kristof <98750722+kulacs-kristof@users.noreply.github.com>
2023-06-18 22:10:54 +0200

    Add files via upload

---
+++ Kulacs_Kristóf_9A_Tisza_49p_10h/tiszato.js
@@ -0,0 +1,12 @@
+function turizmus(melyik){
+    document.getElementById('turizmusszovegkeret').style.display='none';
+    // hiba
+    document.getElementById('turizmuskep').src=kepek/+'.jpg';
+    document.getElementById('turizmuskepkeret').style.display='block';   
+}
+
+// hiba
+function keprejt(melyik){
+  document.getElementById('turizmuskepkeret').display='none';
+  document.getElementById('turizmusszovegkeret').display='block';    
+}
